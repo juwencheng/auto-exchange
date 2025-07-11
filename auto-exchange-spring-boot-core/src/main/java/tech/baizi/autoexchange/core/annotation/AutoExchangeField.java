2afd3c8bc51260b702e4aeefec0b5574cb8f4fea
juwencheng <juwencheng@baiziio.com>
2025-07-11 16:55:48 +0800

    feat: 将注解AutoExchange修改为AutoExchangeField，更清晰的表达使用位置

---
+++ auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/core/annotation/AutoExchangeField.java
@@ -0,0 +1,24 @@
+package tech.baizi.autoexchange.core.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface AutoExchangeField {
+    /**
+     * 在返回的JSON中，新增的汇率信息对象的字段名。
+     * 这是value()的别名。
+     * <b>注意：</b>不能和原字段名字一样，一样会覆盖原来字段。
+     */
+    @AliasFor("value")
+    String name() default "";
+
+    @AliasFor("name")
+    String value() default "";
+}
