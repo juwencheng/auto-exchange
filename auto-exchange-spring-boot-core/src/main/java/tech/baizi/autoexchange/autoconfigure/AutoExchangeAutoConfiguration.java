a84e2155bea39707afa38d641a690e3101123e35
juwencheng <juwencheng@baiziio.com>
2025-07-11 17:03:43 +0800

    feat: 默认使用AutoApplyExchangeStrategy模式，也可手动指定

--- auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/autoconfigure/AutoExchangeAutoConfiguration.java
+++ auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/autoconfigure/AutoExchangeAutoConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import tech.baizi.autoexchange.aspect.AutoExchangeAspect;
+import tech.baizi.autoexchange.core.strategy.AutoApplyExchangeStrategy;
 import tech.baizi.autoexchange.core.support.TypedResultWrapper;
 import tech.baizi.autoexchange.core.support.TypedResultWrapperSerializer;
 import tech.baizi.autoexchange.provider.IDataProvider;
@@ -25,20 +26,6 @@ import tech.baizi.autoexchange.core.strategy.InPlaceApplyExchangeStrategy;
 @ConditionalOnClass({RestController.class})
 @EnableConfigurationProperties({AutoExchangeProperties.class})
 public class AutoExchangeAutoConfiguration {
-
-    /**
-     * 处理ObjectMapper的Bean
-     * <p>
-     * 如果用户的Spring上下文中已经存在一个ObjectMapper的Bean，此方法将不会被执行。
-     * 如果不存在，Spring将调用此方法创建一个默认的ObjectMapper Bean，并放入上下文中。
-     * 这样，我们后续的Aspect就能确保总有一个ObjectMapper可用。
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
-
 
     @Bean
     @ConditionalOnMissingBean(name = "autoExchangeDataProvider")
@@ -54,23 +41,10 @@ public class AutoExchangeAutoConfiguration {
     }
 
     // ------------- 注册应用汇率的策略方法类 ------
-
-    @Bean
-    @ConditionalOnProperty(prefix = "auto.exchange", name = "mode", havingValue = "INPLACE", matchIfMissing = false)
-    public IApplyExchangeStrategy inPlaceApplyExchangeStrategy() {
-        return new InPlaceApplyExchangeStrategy();
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "auto.exchange", name = "mode", havingValue = "APPEND", matchIfMissing = false)
-    public IApplyExchangeStrategy applyExchangeStrategy() {
-        return new AppendApplyExchangeStrategy();
-    }
-
     @Bean
     @ConditionalOnMissingBean
     public IApplyExchangeStrategy autoExchangeStrategy() {
-        return new InPlaceApplyExchangeStrategy();
+        return new AutoApplyExchangeStrategy(new InPlaceApplyExchangeStrategy(), new AppendApplyExchangeStrategy());
     }
 
     @Bean
