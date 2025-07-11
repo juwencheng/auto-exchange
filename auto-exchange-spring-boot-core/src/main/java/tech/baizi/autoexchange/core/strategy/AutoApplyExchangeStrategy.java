40a34276f122ba7f94e3f923afce5bd19429a957
juwencheng <juwencheng@baiziio.com>
2025-07-11 17:00:51 +0800

    feat: 手动指定模式升级成为自动推断，使用AutoApplyExchangeStrategy来支持，注意，不支持混合模式，即对象使用了INPLACE模式，对象属性使用注解模式。

---
+++ auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/core/strategy/AutoApplyExchangeStrategy.java
@@ -0,0 +1,50 @@
+package tech.baizi.autoexchange.core.strategy;
+
+
+import tech.baizi.autoexchange.core.strategy.meta.ClassMetadata;
+
+import java.util.*;
+
+public class AutoApplyExchangeStrategy extends AbstractApplyExchangeStrategy implements IApplyExchangeStrategy {
+    private final InPlaceApplyExchangeStrategy inPlaceApplyExchangeStrategy;
+    private final AppendApplyExchangeStrategy appendApplyExchangeStrategy;
+
+    public AutoApplyExchangeStrategy(InPlaceApplyExchangeStrategy inPlaceApplyExchangeStrategy, AppendApplyExchangeStrategy appendApplyExchangeStrategy) {
+        this.inPlaceApplyExchangeStrategy = inPlaceApplyExchangeStrategy;
+        this.appendApplyExchangeStrategy = appendApplyExchangeStrategy;
+    }
+
+    @Override
+    protected Object createPlaceholder(Object object) {
+        if (object instanceof Collection) {
+            return new LinkedList<>();
+        } else if (object instanceof Map) {
+            return new HashMap<>(((Map<?, ?>) object).size());
+        }
+        ClassMetadata classMetadata = getClassMetadata(object.getClass());
+
+        if (classMetadata == null) {
+            classMetadata = buildClassMetadata(object.getClass());
+        };
+        if (classMetadata.isApplyExchangeImplementor()) {
+            return object;
+        }
+        return new LinkedHashMap<>();
+    }
+
+    @Override
+    protected Object decideNodeTransformation(Object originalObject, Map<String, Object> processedProperties, boolean hasChildrenChanged) {
+        ClassMetadata classMetadata = getClassMetadata(originalObject.getClass());
+        if (classMetadata == null) return originalObject;
+        boolean isApplyImplementor = classMetadata.isApplyExchangeImplementor();
+
+        if (isApplyImplementor) {
+            if (hasChildrenChanged) {
+                throw new IllegalStateException("对象 " + originalObject.getClass().getName() +
+                        " 实现了IApplyExchange (In-place模式), 但其子节点被转换为了Map (Append模式), 这是不被支持的混合用法。");
+            }
+            return inPlaceApplyExchangeStrategy.decideNodeTransformation(originalObject, processedProperties, hasChildrenChanged);
+        }
+        return appendApplyExchangeStrategy.decideNodeTransformation(originalObject, processedProperties, hasChildrenChanged);
+    }
+}
