15a1fc2eb0dd882fd208d4642802c7c772378fa4
juwencheng <juwencheng@baiziio.com>
2025-07-13 10:43:27 +0800

    feat: 优化注释

--- auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/aspect/AutoExchangeAspect.java
+++ auto-exchange-spring-boot-core/src/main/java/tech/baizi/autoexchange/aspect/AutoExchangeAspect.java
@@ -17,9 +17,8 @@ import tech.baizi.autoexchange.core.strategy.IApplyExchangeStrategy;
 
 import javax.servlet.http.HttpServletRequest;
 
-// 关键：设置一个非常低的优先级（即一个非常大的数值）
+// 设置一个非常低的优先级（即一个非常大的数值）
 // 这能确保我们的切面在大多数其他切面（如@Transactional, @Cacheable）之后执行
-// 从而避免我们改变返回类型后，影响到期望接收原始DTO的其他切面。
 @Aspect
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class AutoExchangeAspect {
